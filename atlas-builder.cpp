9afe82a35122966b8b916ac269e1786dc7146378
Eduardo Costa <m4c0@users.noreply.github.com>
2025-08-07 14:02:40 +0100

    atlas-builder: first attempt

--- atlas-builder.cpp
+++ atlas-builder.cpp
@@ -1,8 +1,29 @@
 #pragma leco tool
 
+import dotz;
 import jute;
 import pprent;
 import print;
+import stubby;
+
+static stbi::pixel out[1024 * 1024];
+
+static unsigned next_id = 0;
+
+static void process(jute::view file) {
+  dotz::ivec2 id { next_id % 64, next_id / 64 };
+  stbi::load(file, nullptr, [&](auto, auto & img) {
+    auto data = reinterpret_cast<const stbi::pixel *>(&*img.data);
+    auto sp = id * 16;
+    for (auto y = 0; y < 16; y++) {
+      auto yp = out + (sp.y + y) * 1024 + sp.x;
+      auto yo = data + y * 16;
+      for (auto x = 0; x < 16; x++) yp[x] = yo[x];
+    }
+  });
+
+  next_id++;
+}
 
 static void recurse(const char * dir) {
   auto dirv = jute::view::unsafe(dir);
@@ -11,11 +32,12 @@ static void recurse(const char * dir) {
     auto dv = jute::view::unsafe(d);
     auto p = (dirv + "/" + dv).cstr();
     jute::view pv { p };
-    if (pv.ends_with(".png")) putln(p);
+    if (pv.ends_with(".png")) process(pv);
     if (dv.split('.').after == "") recurse(p.begin());
   }
 }
 
 int main() {
   recurse("PixeLike2_AssetPack");
+  stbi::write_rgba_unsafe("sprites/pixelite2.png", 1024, 1024, out);
 }
