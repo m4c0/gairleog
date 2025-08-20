a00cf2882a86177391cf50d7176fb8f9960937a2
Eduardo Costa <m4c0@users.noreply.github.com>
2025-08-20 20:54:04 +0100

    v: splitting logic

--- poc.cpp
+++ poc.cpp
@@ -1,126 +1,26 @@
 #pragma leco app
-#pragma leco add_resource_dir sprites
-#pragma leco add_shader gairleog.frag
-#pragma leco add_shader gairleog.vert
 
-import casein;
 import dotz;
 import jute;
-import sires;
 import sprdef;
-import traits;
-import wagen;
-import vinyl;
-import voo;
-
-static constexpr const auto max_sprites = 10240;
-
-struct sprite {
-  dotz::vec2 pos;
-  dotz::vec2 uv;
-};
-
-struct upc {
-  dotz::vec2 grid_pos;
-  dotz::vec2 grid_size;
-};
-
-struct app_stuff {
-  voo::device_and_queue dq { "gairleog", casein::native_ptr };
-  vee::render_pass rp = voo::single_att_render_pass(dq);
-  voo::single_frag_dset dset { 1 };
-  vee::pipeline_layout pl = vee::create_pipeline_layout(
-      dset.descriptor_set_layout(),
-      vee::vertex_push_constant_range<upc>());
-  vee::gr_pipeline ppl = vee::create_graphics_pipeline({
-    .pipeline_layout = *pl,
-    .render_pass = *rp,
-    .shaders {
-      voo::shader("gairleog.vert.spv").pipeline_vert_stage(),
-      voo::shader("gairleog.frag.spv").pipeline_frag_stage(),
-    },
-    .bindings {
-      voo::one_quad::vertex_input_bind(),
-      vee::vertex_input_bind_per_instance(sizeof(sprite)),
-    },
-    .attributes {
-      voo::one_quad::vertex_attribute(0),
-      vee::vertex_attribute_vec2(1, traits::offset_of(&sprite::pos)),
-      vee::vertex_attribute_vec2(1, traits::offset_of(&sprite::uv)),
-    },
-  });
-  vee::sampler smp = [] {
-    auto info = vee::sampler_create_info {};
-    info.address_mode(VK_SAMPLER_ADDRESS_MODE_CLAMP_TO_EDGE);
-    info.nearest();
-    info.unnormalizedCoordinates = wagen::vk_true;
-    return vee::create_sampler(info);
-  }();
-  voo::one_quad oq { dq };
-  voo::bound_buffer buf = voo::bound_buffer::create_from_host(
-      dq.physical_device(),
-      max_sprites * sizeof(sprite),
-      vee::buffer_usage::vertex_buffer);
-  voo::bound_image img {};
-  unsigned count {};
-} * g_as;
-
-struct ext_stuff {
-  voo::swapchain_and_stuff sw { g_as->dq, *g_as->rp };
-} * g_es;
+import v;
 
 [[nodiscard]] constexpr auto uv(unsigned i) {
   return dotz::ivec2 { i % 64, i / 64 };
 }
+[[nodiscard]] auto uv(jute::view id, unsigned idx = 0) {
+  static const auto sprdefs = sprdef::load();
+  return uv(sprdefs[id] + idx);
+}
 
-const int i = [] {
-  using namespace vinyl;
-  on(START,  [] {
-    g_as = new app_stuff {};
-
-    auto img = sires::real_path_name("pixelite2.png");
-    voo::load_image(img, g_as->dq.physical_device(), g_as->dq.queue(), &g_as->img, [] {
-      vee::update_descriptor_set(g_as->dset.descriptor_set(), 0, i, *g_as->img.iv, *g_as->smp);
-    });
-
-    auto sprdefs = sprdef::load();
-
+  /*
     voo::memiter<sprite> m { *g_as->buf.memory, &g_as->count };
-    m += { .pos { 2, 1 } };
-    m += { .uv = uv(sprdefs["environment/props/mushroom1"]) };
-  });
-  on(RESIZE, [] { delete g_es; g_es = nullptr; });
-  on(FRAME,  [] {
-    if (!g_es) g_es = new ext_stuff {};
-
-    auto q = g_as->dq.queue();
-
-    g_es->sw.acquire_next_image();
-    g_es->sw.queue_one_time_submit(q, [] {
-      auto cb = g_es->sw.command_buffer();
-      auto ext = g_es->sw.extent();
-
-      upc pc {
-        .grid_pos { 8 },
-        .grid_size = dotz::vec2 { g_es->sw.aspect(), 1.0f } * 8,
-      };
-
-      auto rp = g_es->sw.cmd_render_pass({
-        .clear_colours { vee::clear_colour(0, 0, 0, 1) },
-      });
-      vee::cmd_set_viewport(cb, ext);
-      vee::cmd_set_scissor(cb, ext);
-      vee::cmd_bind_gr_pipeline(cb, *g_as->ppl);
-      vee::cmd_bind_descriptor_set(cb, *g_as->pl, 0, g_as->dset.descriptor_set());
-      vee::cmd_push_vertex_constants(cb, *g_as->pl, &pc);
-      vee::cmd_bind_vertex_buffers(cb, 1, *g_as->buf.buffer);
-      g_as->oq.run(cb, 0, g_as->count);
-    });
-    g_es->sw.queue_present(q);
-  });
-  on(STOP,   [] {
-    delete g_es;
-    delete g_as;
-  });
-  return 0;
-}();
+    m += { .pos { 2, 1 }, .uv = uv("environment/walls/brick_clay", 3) };
+    m += { .pos { 2, 2 }, .uv = uv("environment/walls/brick_clay", 14) };
+    m += { .pos { 2, 3 }, .uv = uv("environment/walls/brick_clay", 19) };
+    m += { .pos { 2, 4 }, .uv = uv("environment/walls/brick_clay", 25) };
+    m += { .pos { 1, 3 }, .uv = uv("environment/walls/brick_clay", 33) };
+    m += { .pos { 3, 3 }, .uv = uv("environment/walls/brick_clay", 34) };
+    m += { .pos { 4, 3 }, .uv = uv("environment/walls/brick_clay", 35) };
+    m += { .uv = uv("environment/props/mushroom1") };
+    */
