8629eab6c7d0b8a662369dd55e72a2e5973e9a39
Eduardo Costa <m4c0@users.noreply.github.com>
2025-09-10 18:24:20 +0100

    tiledef: reading most of the file

--- tiledefs-checker.cpp
+++ tiledefs-checker.cpp
@@ -8,28 +8,45 @@ import lispy;
 import print;
 
 struct tiledef {
-  unsigned light {};
+  bool block;
+  int light {};
   hai::array<unsigned> sprite {};
 };
-
-static constexpr const auto eval = lispy::eval<lispy::node>;
 
 int main() try {
   auto src = jojo::read_cstr("tiledefs.lsp");
 
+  struct node : lispy::node {
+    enum { t_empty, t_block, t_light, t_spr } type {};
+    tiledef tdef {};
+  };
   struct context : lispy::context {
     hashley::fin<tiledef> tiledefs { 127 };
   } ctx {
-    { .allocator = lispy::allocator<>() },
+    { .allocator = lispy::allocator<node>() },
   };
+
+  // static constexpr const auto clone = lispy::clone<node>;
+  static constexpr const auto eval = lispy::eval<node>;
+
   ctx.fns["block"] = [](auto ctx, auto n, auto aa, auto as) -> const lispy::node * {
-    return n;
+    if (as != 0) lispy::err(n, "block does not accept parameters");
+    return new (ctx.allocator()) node { *n, node::t_block, { .block = true } };
   };
   ctx.fns["light"] = [](auto ctx, auto n, auto aa, auto as) -> const lispy::node * {
-    return n;
+    if (as != 1) lispy::err(n, "light requires intensity as parameter");
+    auto i = lispy::to_i(eval(ctx, aa[0]));
+    if (i < 0 || i > 15) lispy::err(n, "light intensity should be between 0 and 15");
+    return new (ctx.allocator()) node { *n, node::t_light, { .light = i } };
   };
   ctx.fns["spr"] = [](auto ctx, auto n, auto aa, auto as) -> const lispy::node * {
-    return n;
+    if (as == 0) lispy::err(n, "spr requires at least one name");
+
+    auto nn = new (ctx.allocator()) node { *n, node::t_spr, { .sprite { as } } };
+    for (auto i = 0; i < as; i++) {
+      if (!lispy::is_atom(aa[i])) lispy::err(n, "spr expects atom names");
+    }
+    return nn;
   };
   ctx.fns["tiledef"] = [](auto ctx, auto n, auto aa, auto as) -> const lispy::node * {
     if (as < 2) lispy::err(n, "tiledef requires at least name and spr");
