32409480fe0b3b6174588ad4cab7416300448c31
Eduardo Costa <m4c0@users.noreply.github.com>
2025-09-10 18:06:27 +0100

    tiledefs: basic eval

--- tiledefs-checker.cpp
+++ tiledefs-checker.cpp
@@ -12,6 +12,8 @@ struct tiledef {
   hai::array<unsigned> sprite {};
 };
 
+static constexpr const auto eval = lispy::eval<lispy::node>;
+
 int main() try {
   auto src = jojo::read_cstr("tiledefs.lsp");
 
@@ -20,9 +22,23 @@ int main() try {
   } ctx {
     { .allocator = lispy::allocator<>() },
   };
+  ctx.fns["block"] = [](auto ctx, auto n, auto aa, auto as) -> const lispy::node * {
+    return n;
+  };
+  ctx.fns["light"] = [](auto ctx, auto n, auto aa, auto as) -> const lispy::node * {
+    return n;
+  };
+  ctx.fns["spr"] = [](auto ctx, auto n, auto aa, auto as) -> const lispy::node * {
+    return n;
+  };
   ctx.fns["tiledef"] = [](auto ctx, auto n, auto aa, auto as) -> const lispy::node * {
     if (as < 2) lispy::err(n, "tiledef requires at least name and spr");
     if (!lispy::is_atom(aa[0])) lispy::err(n, "tiledef name should be an atom");
+
+    for (auto i = 1; i < as; i++) {
+      auto _ = eval(ctx, aa[i]);
+    }
+
     return n;
   };
   lispy::run(src, ctx);
