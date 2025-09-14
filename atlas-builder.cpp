9f39134e67b82297b56a8e3212626356ba9baf70
Eduardo Costa <m4c0@users.noreply.github.com>
2025-09-14 19:32:21 +0100

    update stubby

--- atlas-builder.cpp
+++ atlas-builder.cpp
@@ -4,6 +4,7 @@
 #endif
 
 import dotz;
+import jojo;
 import jute;
 import hay;
 import pprent;
@@ -19,27 +20,26 @@ static hay<FILE *, sysstd::fopen, fclose> lsp { "sprites/pixelite2.lsp", "wb" };
 static void process(jute::view file) {
   auto fid = file.split('/').after.rsplit('.').before;
   fputln(lsp, "(sprdef ", fid, " ", next_id, ")");
-  stbi::load(file, nullptr, [&](auto, auto & img) {
-    auto data = reinterpret_cast<const stbi::pixel *>(*img.data);
-    // TODO: deal with animations
-    if (img.num_channels != 4) return;
+  auto img = stbi::load(jojo::read_cstr(file));
+  auto data = reinterpret_cast<const stbi::pixel *>(*img.data);
+  // TODO: deal with animations
+  if (img.num_channels != 4) return;
 
-    for (auto e = 0; e < img.height / 16; e++) {
-      for (auto d = 0; d < img.width / 16; d++) {
-        dotz::ivec2 id { next_id % 64, next_id / 64 };
-        auto sp = id * 16;
+  for (auto e = 0; e < img.height / 16; e++) {
+    for (auto d = 0; d < img.width / 16; d++) {
+      dotz::ivec2 id { next_id % 64, next_id / 64 };
+      auto sp = id * 16;
 
-        for (auto y = 0; y < 16; y++) {
-          auto yp = out + (sp.y + y) * 1024 + sp.x;
-          auto yo = data + (y + e * 16) * img.width + d * 16;
-          for (auto x = 0; x < 16; x++) {
-            yp[x] = yo[x];
-          }
+      for (auto y = 0; y < 16; y++) {
+        auto yp = out + (sp.y + y) * 1024 + sp.x;
+        auto yo = data + (y + e * 16) * img.width + d * 16;
+        for (auto x = 0; x < 16; x++) {
+          yp[x] = yo[x];
         }
-        next_id++;
       }
+      next_id++;
     }
-  });
+  }
 }
 
 static void recurse(const char * dir) {
