c4abf0a98a33b361afa220be96ab5d7cd42a3185
Eduardo Costa <m4c0@users.noreply.github.com>
2025-09-17 17:11:21 +0100

    map: taking "render"

--- map.cppm
+++ map.cppm
@@ -1,6 +1,7 @@
 #pragma leco add_impl map_builder
 #pragma leco add_impl map_walls
 export module map;
+import dotz;
 
 export struct map {
   static constexpr const auto wall = 0x400'0000;
@@ -11,4 +12,14 @@ export struct map {
   unsigned data[h][w] {};
 
   void build();
+
+  void load(auto m) {
+    for (auto y = 0; y < h; y++) {
+      for (auto x = 0; x < w; x++) {
+        auto c = data[y][x];
+        dotz::ivec2 uv { c % 64, c / 64 };
+        m->push({ .pos { x, y }, .uv = uv });
+      }
+    }
+  }
 };
--- poc.cpp
+++ poc.cpp
@@ -1,36 +1,17 @@
 #pragma leco app
 
 import casein;
-import dotz;
-import jute;
-import hai;
-import hashley;
 import map;
 import res;
 import silog;
 import sires;
-import traits;
 import v;
-
-[[nodiscard]] constexpr auto uv(unsigned i) {
-  return dotz::ivec2 { i % 64, i / 64 };
-}
 
 static map g_map {};
 
-static void load(auto m) {
-  for (auto y = 0; y < g_map.h; y++) {
-    for (auto x = 0; x < g_map.w; x++) {
-      auto c = g_map.data[y][x];
-      m->push({ .pos { x, y }, .uv = uv(c) });
-    }
-  }
-}
-
 static void on_frame() {
   g_map.build();
-
-  load(v::map());
+  g_map.load(v::map());
   v::on_frame = [] {};
 
   casein::handle(casein::KEY_DOWN, casein::K_SPACE, on_frame);
