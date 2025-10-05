f7a44b6c51f1f324d4ca7e96f4bfc0626cfb601d
Eduardo Costa <m4c0@users.noreply.github.com>
2025-10-05 13:30:20 +0100

    TODO

--- res-checker.cpp
+++ res-checker.cpp
@@ -7,6 +7,7 @@ int main() try {
   int res = 0;
   res::on_error([&](auto msg) { errln(msg); res = 1; });
   res::load_locally([] {
+    // TODO: create strategies to test each resource throughly
     roomdefs::for_size(2, 2);
     roomdefs::for_size(8, 6);
   });
