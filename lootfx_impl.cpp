b436ff0da1fa5e544cf7777c018a5764e4a1310c
Eduardo Costa <m4c0@users.noreply.github.com>
2025-10-30 22:18:36 +0000

    lootfx: lispy boilerplate

--- lootfx_impl.cpp
+++ lootfx_impl.cpp
@@ -1,6 +1,18 @@
 module lootfx;
+import lispy;
+
+using namespace lispy;
+using namespace lispy::experimental;
 
 namespace lootfx {
-  void run(jute::view src) {
+  void run(jute::view src) try {
+    basic_context<node> ctx {};
+    ctx.fns["fx"] = [](auto n, auto aa, auto as) -> const node * {
+      return n;
+    };
+
+    ctx.run(src);
+  } catch (const parser_error & e) {
+    throw to_file_err("lootfx.lsp", e);
   }
 }
