063496626edcc57a2b74fa496a75727bc1a30ae4
Eduardo Costa <m4c0@users.noreply.github.com>
2025-10-31 18:47:31 +0000

    lootfx: trying a diff lispy approach

--- lootfx_impl.cpp
+++ lootfx_impl.cpp
@@ -1,29 +1,28 @@
 module lootfx;
+import hai;
+import hashley;
 import lispy;
 
 using namespace lispy;
 using namespace lispy::experimental;
 
 namespace lootfx {
+  struct {
+    hai::fn<node *> alloc {};
+    hashley::fin<const node *> nodes { 127 };
+  } data {};
+
   void run(jute::view src) try {
-    basic_context<node> ctx {};
+    data = {};
+    data.alloc = allocator<node>();
+
+    basic_context<node> ctx { data.alloc };
     ctx.fns["fx"] = [](auto n, auto aa, auto as) -> const node * {
       if (as != 2) err("expecting a name and an action");
       if (!is_atom(aa[0])) err("expecting an atom as the name");
-
-      basic_context<node> ctx { n->ctx->allocator };
-      ctx.fns["heal"] = [](auto n, auto aa, auto as) -> const node * {
-        return n;
-      };
-      ctx.fns["str"] = [](auto n, auto aa, auto as) -> const node * {
-        return n;
-      };
-      ctx.eval(aa[1]);
-
-      auto nn = clone<node>(n);
-      return nn;
+      data.nodes[aa[0]->atom] = aa[1];
+      return n;
     };
-
     ctx.run(src);
   } catch (const parser_error & e) {
     throw to_file_err("lootfx.lsp", e);
