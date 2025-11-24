d20d66119b13a1e67317ce0cec923fcd4ea62e11
Eduardo Costa <m4c0@users.noreply.github.com>
2025-11-24 12:52:29 +0000

    save: initial check implementation

--- save_impl.cpp
+++ save_impl.cpp
@@ -1,5 +1,11 @@
 module save;
+import buoy;
+import jojo;
+
+static const auto file = buoy::path("gairleog", "save.dat");
 
 void save::prefetch(hai::fn<void> callback) {
-  callback();
+  jojo::read(file, nullptr, [=](void *, hai::cstr & file) mutable {
+    callback();
+  });
 }
