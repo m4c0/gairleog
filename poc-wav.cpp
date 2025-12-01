f44fed5b84c00c2b5304c0faaedefbd3da109ea0
Eduardo Costa <m4c0@users.noreply.github.com>
2025-12-01 14:14:26 +0000

    wav reading as a module

--- poc-wav.cpp
+++ poc-wav.cpp
@@ -4,76 +4,17 @@ import jojo;
 import jute;
 import print;
 import sv;
-
-struct error {
-  jute::heap msg;
-};
-
-[[noreturn]] static void erred(sv msg) {
-  throw error { .msg { msg } };
-}
-
-class reader {
-  const void * m_ptr;
-  const void * m_end;
-
-public:
-  explicit reader(sv data) :
-    m_ptr { data.begin() }
-  , m_end { data.end() }
-  {}
-
-  template<typename T> [[nodiscard]] T read() {
-    auto s = static_cast<const T *>(m_ptr);
-    auto e = static_cast<const T *>(m_end);
-    auto n = s + 1;
-    if (n > e) erred("Premature EOF");
-    m_ptr = n;
-    return *s;
-  }
-  template<typename T> void skip() {
-    auto _ = read<T>();
-  }
-  void skip(unsigned size) {
-    auto s = static_cast<const char *>(m_ptr);
-    auto e = static_cast<const char *>(m_end);
-    auto n = s + size;
-    if (n > e) erred("Premature EOF");
-    m_ptr = n;
-  }
-};
-
-static void run(sv data) {
-  reader r { data };
-  if (r.read<unsigned>() != 'FFIR') erred("Invalid WAV file");
-  if (r.read<unsigned>() != data.size() - 8) erred("WAV file size does not match");
-  if (r.read<unsigned>() != 'EVAW') erred("Unsupported RIFF file");
-
-  if (r.read<unsigned>() != ' tmf') erred("WAV files should have a FMT chunk");
-  if (r.read<unsigned>() != 16) erred("Unsupported WAV FMT size");
-  if (r.read<short>() != 1) erred("Only PCM WAV is supported");
-  auto channels = r.read<short>();
-  if (channels != 1 && channels != 2) erred("Invalid number of channels in WAV");
-  if (r.read<unsigned>() != 44100) erred("Expecting WAV with sample rate of 44100");
-  r.skip<unsigned>(); // dwAvgBytesPerSec
-  r.skip<short>(); // wBlockAlign
-  if (r.read<short>() != 24) erred("Only 24 bits per sample is supported");
-
-  while (r.read<unsigned>() != 'atad') {
-    r.skip(r.read<unsigned>());
-  }
-
-  auto sz = r.read<unsigned>();
-  auto smps = sz / (3 * 2);
-  putln("Data with size ", sz);
-  putln("Num samples = ", smps);
-  putln("Length = ", smps / 44100.f);
-}
+import wav;
 
 int main() try {
   auto data = jojo::slurp("sfx/01_human_atk_sword_1.wav");
-  run(data);
-} catch (const error & e) {
+  auto wav = wav::run(data);
+
+  auto smps = wav.size() / (3 * 2);
+  putln("Data with size ", wav.size());
+  putln("Num samples = ", smps);
+  putln("Length = ", smps / 44100.f);
+} catch (const wav::error & e) {
   errln(e.msg);
   return 1;
 }
---
+++ wav.cppm
@@ -0,0 +1,76 @@
+export module wav;
+import jute;
+import sv;
+
+namespace wav {
+  export struct error {
+    jute::heap msg;
+  };
+}
+
+[[noreturn]] static void erred(sv msg) {
+  throw wav::error { .msg { msg } };
+}
+
+class reader {
+  const void * m_ptr;
+  const void * m_end;
+
+public:
+  explicit reader(sv data) :
+    m_ptr { data.begin() }
+  , m_end { data.end() }
+  {}
+
+  template<typename T> [[nodiscard]] T read() {
+    auto s = static_cast<const T *>(m_ptr);
+    auto e = static_cast<const T *>(m_end);
+    auto n = s + 1;
+    if (n > e) erred("Premature EOF");
+    m_ptr = n;
+    return *s;
+  }
+  template<typename T> void skip() {
+    auto _ = read<T>();
+  }
+  void skip(unsigned size) {
+    auto s = static_cast<const char *>(m_ptr);
+    auto e = static_cast<const char *>(m_end);
+    auto n = s + size;
+    if (n > e) erred("Premature EOF");
+    m_ptr = n;
+  }
+
+  constexpr auto ptr() const { return static_cast<const char *>(m_ptr); }
+};
+
+static sv run(sv data) {
+  reader r { data };
+  if (r.read<unsigned>() != 'FFIR') erred("Invalid WAV file");
+  if (r.read<unsigned>() != data.size() - 8) erred("WAV file size does not match");
+  if (r.read<unsigned>() != 'EVAW') erred("Unsupported RIFF file");
+
+  if (r.read<unsigned>() != ' tmf') erred("WAV files should have a FMT chunk");
+  if (r.read<unsigned>() != 16) erred("Unsupported WAV FMT size");
+  if (r.read<short>() != 1) erred("Only PCM WAV is supported");
+  auto channels = r.read<short>();
+  if (channels != 1 && channels != 2) erred("Invalid number of channels in WAV");
+  if (r.read<unsigned>() != 44100) erred("Expecting WAV with sample rate of 44100");
+  r.skip<unsigned>(); // dwAvgBytesPerSec
+  r.skip<short>(); // wBlockAlign
+  if (r.read<short>() != 24) erred("Only 24 bits per sample is supported");
+
+  while (r.read<unsigned>() != 'atad') {
+    r.skip(r.read<unsigned>());
+  }
+
+  auto sz = r.read<unsigned>();
+  auto p = r.ptr();
+  if (p + sz > data.end()) erred("Data in WAV overflows");
+  return { p, sz };
+}
+
+namespace wav {
+  export sv run(sv data) { return ::run(data); }
+}
+
