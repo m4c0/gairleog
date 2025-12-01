f5c10cb4edf57bf7db78766643bada126322c9cc
Eduardo Costa <m4c0@users.noreply.github.com>
2025-12-01 14:49:05 +0000

    poc-wav: got a jump-scare audio output

--- poc-wav.cpp
+++ poc-wav.cpp
@@ -1,17 +1,25 @@
 #pragma leco test
 
 import jojo;
-import jute;
 import print;
-import sv;
+import siaudio;
 import wav;
 
 int main() try {
-  auto data = jojo::slurp("sfx/01_human_atk_sword_1.wav");
-  auto wav = wav::load(data);
+  static auto wav = wav::load(jojo::slurp("sfx/01_human_atk_sword_1.wav"));
+  static unsigned ptr = 0;
 
   putln("Num samples = ", wav.size());
   putln("Length = ", wav.size() / 44100.f);
+
+  siaudio::filler([](float * data, unsigned samples) {
+    for (auto i = 0; i < samples; i++) {
+      *data++ = wav[ptr++];
+    }
+  });
+  siaudio::rate(44100);
+
+  while (ptr < wav.size()) { /* wait */ }
 } catch (const wav::error & e) {
   errln(e.msg);
   return 1;
