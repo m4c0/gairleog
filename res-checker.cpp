b4dc7d96fc960d04695e9806bf093854962bf278
Eduardo Costa <m4c0@users.noreply.github.com>
2025-12-01 23:28:21 +0000

    validate entdef using player data

--- res-checker.cpp
+++ res-checker.cpp
@@ -1,4 +1,5 @@
 #pragma leco test
+import entdefs;
 import errs;
 import hai;
 import hitdefs;
@@ -11,6 +12,10 @@ int main() try {
     // TODO: create strategies to test each resource throughly
     roomdefs::for_size(2, 2);
     roomdefs::for_size(8, 6);
+
+    auto e = entdefs::get("player");
+    assert(e.attack_sprite.id, "missing player attack sprite");
+    assert(e.sfx.attack != "", "missing player attack sound");
   });
 } catch (hai::cstr & e) {
   errln(e);
