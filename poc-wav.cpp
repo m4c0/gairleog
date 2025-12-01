13f947efa11aed737687dcae4ff3c1004c0e2edc
Eduardo Costa <m4c0@users.noreply.github.com>
2025-12-01 13:57:31 +0000

    poc-wav: parsing the remainder of FMT

--- poc-wav.cpp
+++ poc-wav.cpp
@@ -13,19 +13,43 @@ struct error {
   throw error { .msg { msg } };
 }
 
-static void run(sv data) {
-  auto ptr = reinterpret_cast<const unsigned *>(data.begin());
-  if (*ptr++ != 'FFIR') erred("Invalid WAV file");
-  if (*ptr++ != data.size() - 8) erred("WAV file size does not match");
-  if (*ptr++ != 'EVAW') erred("Unsupported RIFF file");
+class reader {
+  const void * m_ptr;
+  const void * m_end;
 
-  if (*ptr++ != ' tmf') erred("WAV files should have a FMT chunk");
-  if (*ptr++ != 16) erred("Unsupported WAV FMT size");
-  // WORD wFormatTag; // Format category
-  // WORD wChannels; // Number of channels
-  // DWORD dwSamplesPerSec; // Sampling rate
-  // DWORD dwAvgBytesPerSec; // For buffer estimation
-  // WORD wBlockAlign; // Data block size
+public:
+  explicit reader(sv data) :
+    m_ptr { data.begin() }
+  , m_end { data.end() }
+  {}
+
+  template<typename T> [[nodiscard]] T read() {
+    auto s = static_cast<const T *>(m_ptr);
+    auto e = static_cast<const T *>(m_end);
+    auto n = s + 1;
+    if (n > e) erred("Premature EOF");
+    m_ptr = n;
+    return *s;
+  }
+  template<typename T> void skip() {
+    auto _ = read<T>();
+  }
+};
+
+static void run(sv data) {
+  reader r { data };
+  if (r.read<unsigned>() != 'FFIR') erred("Invalid WAV file");
+  if (r.read<unsigned>() != data.size() - 8) erred("WAV file size does not match");
+  if (r.read<unsigned>() != 'EVAW') erred("Unsupported RIFF file");
+
+  if (r.read<unsigned>() != ' tmf') erred("WAV files should have a FMT chunk");
+  if (r.read<unsigned>() != 16) erred("Unsupported WAV FMT size");
+  if (r.read<short>() != 1) erred("Only PCM WAV is supported");
+  auto channels = r.read<short>();
+  if (channels != 1 && channels != 2) erred("Invalid number of channels in WAV");
+  if (r.read<unsigned>() != 44100) erred("Expecting WAV with sample rate of 44100");
+  r.skip<unsigned>(); // dwAvgBytesPerSec
+  r.skip<short>(); // wBlockAlign
 }
 
 int main() try {
