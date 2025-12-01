8c9d534f20328553fdc3f5f0875094fa3acf118c
Eduardo Costa <m4c0@users.noreply.github.com>
2025-12-01 13:44:33 +0000

    poc-wav: trying to read FMT data

--- poc-wav.cpp
+++ poc-wav.cpp
@@ -1,18 +1,37 @@
 #pragma leco test
 
 import jojo;
+import jute;
+import print;
 import sv;
 
-static bool run(sv data) {
-  auto ptr = reinterpret_cast<const unsigned *>(data.begin());
-  if (*ptr++ != 'FFIR') return false;
-  if (*ptr++ != data.size() - 8) return false;
-  if (*ptr++ != 'EVAW') return false;
+struct error {
+  jute::heap msg;
+};
 
-  return true;
+[[noreturn]] static void erred(sv msg) {
+  throw error { .msg { msg } };
 }
 
-int main() {
+static void run(sv data) {
+  auto ptr = reinterpret_cast<const unsigned *>(data.begin());
+  if (*ptr++ != 'FFIR') erred("Invalid WAV file");
+  if (*ptr++ != data.size() - 8) erred("WAV file size does not match");
+  if (*ptr++ != 'EVAW') erred("Unsupported RIFF file");
+
+  if (*ptr++ != ' tmf') erred("WAV files should have a FMT chunk");
+  if (*ptr++ != 16) erred("Unsupported WAV FMT size");
+  // WORD wFormatTag; // Format category
+  // WORD wChannels; // Number of channels
+  // DWORD dwSamplesPerSec; // Sampling rate
+  // DWORD dwAvgBytesPerSec; // For buffer estimation
+  // WORD wBlockAlign; // Data block size
+}
+
+int main() try {
   auto data = jojo::slurp("sfx/01_human_atk_sword_1.wav");
-  return run(data) ? 0 : 1;
+  run(data);
+} catch (const error & e) {
+  errln(e.msg);
+  return 1;
 }
