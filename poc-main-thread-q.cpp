05df398a712a949eadd165482153d66e38c377a3
Eduardo Costa <m4c0@users.noreply.github.com>
2025-12-19 17:32:57 +0000

    poc-main-thread-q: cond var for consumer

--- poc-main-thread-q.cpp
+++ poc-main-thread-q.cpp
@@ -13,15 +13,19 @@ static fn_t g_queue[queue_size] {};
 static int g_next_prod = 0;
 static int g_next_cons = 0;
 static mtx::mutex g_mtx {};
-static mtx::cond g_cond {};
+static mtx::cond g_cond_cons {};
+static mtx::cond g_cond_prod {};
 
 static fn_t take() {
   mtx::lock l { &g_mtx };
-  if (g_next_prod == g_next_cons) return nullptr;
+  if (g_next_prod == g_next_cons) {
+    g_cond_cons.wait(&l, 1);
+    if (g_next_prod == g_next_cons) return nullptr;
+  }
 
   auto res = g_queue[g_next_cons];
   g_next_cons = (g_next_cons + 1) % queue_size;
-  g_cond.wake_one();
+  g_cond_prod.wake_one();
   return res;
 }
 static void push(fn_t fn) {
@@ -30,19 +34,19 @@ static void push(fn_t fn) {
   auto p = (g_next_prod + 1) % queue_size;
   if (p == g_next_cons) {
     silog::warning("event thread queue is full - this might slow down the whole app");
-    g_cond.wait(&l, 1);
+    g_cond_prod.wait(&l, 1);
     if (p == g_next_cons) silog::die("event thread became too busy - bailing out");
   }
 
   g_queue[g_next_prod] = fn;
   g_next_prod = p;
+  g_cond_cons.wake_one();
 }
 
 void consumer(sith::thread * t) {
   silog::infof("consumer thread %p", t);
   while (!t->interrupted()) {
     // TODO: alarm if task takes too much time
-    // TODO: avoid busy waits
     if (auto r = take()) r();
   }
 }
@@ -72,7 +76,7 @@ int main() {
   sith::stateless_thread t { consumer };
   sith::run_guard g { &t };
 
-  sitime::sleep(1);
+  sitime::sleep(100);
 
   silog::info("joining");
 }
