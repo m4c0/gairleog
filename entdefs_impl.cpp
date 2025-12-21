48cda9759d43bbecc6465bacfa1d7f1e818e8b2f
Eduardo Costa <m4c0@users.noreply.github.com>
2025-12-21 12:19:25 +0000

    entdefs: improving performance
    * uses standard node instead of custom
    * removes extra arena

--- entdefs_impl.cpp
+++ entdefs_impl.cpp
@@ -15,10 +15,6 @@ namespace entdefs {
   };
   hashley::fin<entdef> defs { 127 };
 
-  struct cnode : node, t {
-    void (*attr)(cnode *, const cnode *);
-  };
-
   static float to_light(const lispy::node * n) {
     auto i = lispy::to_f(n);
     if (i < 0 || i > 1) lispy::erred(n, "light intensity should be between 0 and 1");
@@ -44,37 +40,49 @@ namespace entdefs {
     return sprdef::get(name->atom);
   }
 
+  template<auto Attr, auto Fn>
+  static constexpr const node * conv(const node * n, const node * const * aa, unsigned as) {
+    if (as != 1) erred(n, "Expecting a single parameter");
+    auto * obj = static_cast<t *>(context()->ptr("t"));
+    obj->*Attr = Fn(eval<node>(aa[0]));
+    return n;
+  }
+
   template<flags Flags>
-  static constexpr const auto mem_set = lispy::experimental::mem_set<&cnode::attr, [](auto * self, auto * n) {
+  static constexpr const node * set(const node * n, const node * const * aa, unsigned as) {
+    if (as != 0) erred(n, "Expecting no parameters");
+    auto * obj = static_cast<t *>(context()->ptr("t"));
+
     u32flags u {};
-    u.u32 = bit_of(Flags) | bit_of(self->flags);
-    self->flags = u.f;
-  }>;
+    u.u32 = bit_of(Flags) | bit_of(obj->flags);
+    obj->flags = u.f;
+
+    return n;
+  }
 
   static auto entdef_ctx = [] {
     auto ctx = lispy::frame::make();
-    ctx->fns["atkspr"]   = mem_fn<&cnode::attr, &cnode::attack_sprite, to_spr_pair>;
-    ctx->fns["defense"]  = mem_fn<&cnode::attr, &cnode::defense,       to_i>;
-    ctx->fns["life"]     = mem_fn<&cnode::attr, &cnode::max_life,      to_life>;
-    ctx->fns["light"]    = mem_fn<&cnode::attr, &cnode::light,         to_light>;
-    ctx->fns["loot"]     = mem_fn<&cnode::attr, &cnode::loot,          to_loot>;
-    ctx->fns["splatspr"] = mem_fn<&cnode::attr, &cnode::splat_sprite,  to_spr_pair>;
-    ctx->fns["spr"]      = mem_fn<&cnode::attr, &cnode::sprite,        to_spr>;
-    ctx->fns["strength"] = mem_fn<&cnode::attr, &cnode::strength,      to_i>;
+    ctx->fns["atkspr"]   = conv<&t::attack_sprite, to_spr_pair>;
+    ctx->fns["defense"]  = conv<&t::defense,       to_i>;
+    ctx->fns["life"]     = conv<&t::max_life,      to_life>;
+    ctx->fns["light"]    = conv<&t::light,         to_light>;
+    ctx->fns["loot"]     = conv<&t::loot,          to_loot>;
+    ctx->fns["splatspr"] = conv<&t::splat_sprite,  to_spr_pair>;
+    ctx->fns["spr"]      = conv<&t::sprite,        to_spr>;
+    ctx->fns["strength"] = conv<&t::strength,      to_i>;
 
-    ctx->fns["ceramic"]     = mem_set<{ .ceramic     = true }>;
-    ctx->fns["enemy"]       = mem_set<{ .enemy       = true }>;
-    ctx->fns["exit"]        = mem_set<{ .exit        = true }>;
-    ctx->fns["fluorescent"] = mem_set<{ .fluorescent = true }>;
-    ctx->fns["food"]        = mem_set<{ .food        = true }>;
-    ctx->fns["player"]      = mem_set<{ .player      = true }>;
-    ctx->fns["poison"]      = mem_set<{ .poison      = true }>;
-    ctx->fns["solid"]       = mem_set<{ .solid       = true }>;
-    ctx->fns["wall"]        = mem_set<{ .wall        = true }>;
+    ctx->fns["ceramic"]     = set<{ .ceramic     = true }>;
+    ctx->fns["enemy"]       = set<{ .enemy       = true }>;
+    ctx->fns["exit"]        = set<{ .exit        = true }>;
+    ctx->fns["fluorescent"] = set<{ .fluorescent = true }>;
+    ctx->fns["food"]        = set<{ .food        = true }>;
+    ctx->fns["player"]      = set<{ .player      = true }>;
+    ctx->fns["poison"]      = set<{ .poison      = true }>;
+    ctx->fns["solid"]       = set<{ .solid       = true }>;
+    ctx->fns["wall"]        = set<{ .wall        = true }>;
     return ctx;
   }();
 
-  static auto src_arena = arena<cnode>::make();
   static auto src_ctx = [] {
     auto ctx = lispy::frame::make();
     ctx->fns["entdef"] = [](auto n, auto aa, auto as) -> const lispy::node * {
@@ -92,9 +100,10 @@ namespace entdefs {
   }();
 
   void run(jute::view src) {
+    static auto src_arena = arena<node>::make();
     auto a = src_arena->use();
     frame_guard c { src_ctx };
-    lispy::run<cnode>("entdefs.lsp", src);
+    lispy::run<node>("entdefs.lsp", src);
   }
 
   bool has(jute::view name) {
@@ -103,11 +112,16 @@ namespace entdefs {
   t get(jute::view name) {
     auto & d = defs[name];
 
-    temp_arena<cnode> a {};
     frame_guard c1 { src_ctx };
     frame_guard c2 { entdef_ctx };
-    auto res = *fill_clone<cnode>(d.n, d.args.begin(), d.args.size());
-    res.name = jute::heap { name };
+    temp_frame c {};
+
+    t res {
+      .name { name },
+    };
+    c.ptrs["t"] = &res;
+
+    for (const node * arg : d.args) auto _ = eval<node>(arg);
     return res;
   }
 }
