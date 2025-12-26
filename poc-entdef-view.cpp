11fee1333125041bcbea4ccf084b7a4a7bc71c53
Eduardo Costa <m4c0@users.noreply.github.com>
2025-12-26 16:53:02 +0000

    entdefs: tweaking + validation tool

---
+++ poc-entdef-view.cpp
@@ -0,0 +1,47 @@
+#pragma leco tool
+
+import entdefs;
+import glispy;
+import hai;
+import jojo;
+import jute;
+import print;
+import res;
+import save;
+
+using namespace lispy;
+
+int main() try {
+  hai::varray<jute::heap> names { 128 };
+
+  temp_arena<node> a {};
+  temp_frame ctx {};
+  ctx.ptrs["names"] = &names;
+  ctx.fns["entdef"] = [](auto n, auto aa, auto as) -> const node * {
+    if (as == 0) erred(n, "missing name");
+    if (!is_atom(aa[0])) erred(aa[0], "expecting atom as name");
+
+    auto * ptr = static_cast<hai::varray<jute::heap> *>(context()->ptr("names"));
+    ptr->push_back_doubling(aa[0]->atom);
+    return n;
+  };
+
+  run<node>("entdefs.lsp", jojo::slurp("entdefs.lsp"));
+
+  res::load_locally([] {});
+
+  save::current_stage = 1;
+  glispy::reset();
+
+  auto g = glispy::frame_guard();
+  for (auto n : names) {
+    auto t = entdefs::get(n);
+  }
+} catch (const parser_error & e) {
+  errln(to_file_err(e));
+  return 1;
+} catch (death) {
+  return 2;
+} catch (...) {
+  return 3;
+}
